5ab8715324653baf0a28a5fd7e9430cc14589064
Zach Donsky <zach@cortex-design.com>
2024-02-28 14:40:04 -0500

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+
+module.exports = {
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: {
+                flexbox: "no-2009",
+              },
+              stage: 3,
+              features: {
+                "custom-properties": false,
+              },
+            },
+          ],
+        ]
+      : [
+          "@unocss/postcss",
+          {
+            content: ["**/*.{html,js,ts,jsx,tsx}"],
+          },
+        ],
+};
